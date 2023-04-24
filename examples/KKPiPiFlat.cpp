9b9fcbc52c2cc3fa5f6d2fb82049cd44e1ba6f52
Daniel Cervenkov <daniel.cervenkov@cern.ch>
2023-04-24 14:51:00 +0200

    Format KKPiPiFlat

--- examples/KKPiPiFlat.cpp
+++ examples/KKPiPiFlat.cpp
@@ -1,39 +1,42 @@
 /**
- * This is code that creates N number of flat phase space events, calculates the bin number under the KKpipi binning scheme used by Daniel Cervenkov and Martin Tat, AND the amplitude for each event.
- * 
+ * This is code that creates N number of flat phase space events, calculates the
+ *bin number under the KKpipi binning scheme used by Daniel Cervenkov and Martin
+ *Tat, AND the amplitude for each event.
+ *
  * No input.
- * 
- * Output is a ROOT file which stores the real and imaginary parts of the amplitudes, the 5D phase space point and the bin number. 
- * 
- * 
+ *
+ * Output is a ROOT file which stores the real and imaginary parts of the
+ *amplitudes, the 5D phase space point and the bin number.
+ *
+ *
  **/
 
-#include <array>
-#include <string>
-#include <iostream>
 #include <algorithm>
+#include <array>
+#include <iostream>
+#include <string>
 
-#include "TLorentzVector.h"
-#include "TRandom.h"
-#include "TROOT.h"
 #include "TFile.h"
-#include "TTree.h"
+#include "TLorentzVector.h"
 #include "TMath.h"
+#include "TROOT.h"
+#include "TRandom.h"
+#include "TTree.h"
 
-#include "../include/Generator.h"
-#include "../include/Event.h"
-#include "../include/PiPiPiPiMath.h"
 #include "../include/Amplitude.h"
+#include "../include/Event.h"
+#include "../include/Generator.h"
+#include "../include/PiPiPiPiMath.h"
 #include "../include/Utilities.h"
 
-int main()
-{
+int main() {
 
   // Local variables //
 
   int BinNumber;
 
-  double Damp_Mag, DBARamp_Mag, StrongPhaseDiff, Damp_real, Damp_imag, DBARamp_real, DBARamp_imag, rD, m12;
+  double Damp_Mag, DBARamp_Mag, StrongPhaseDiff, Damp_real, Damp_imag,
+      DBARamp_real, DBARamp_imag, rD, m12;
 
   std::complex<double> Damp;
   std::complex<double> DBARamp;
@@ -52,7 +55,10 @@ int main()
 
   ////// Load binning scheme //////
 
-  TFile fOut("/home/patoc/work/DPhil1/D0_Mixing_Generator_Studies/for_jairus/D02pipipipi_binning_scheme/ROOT_output/2M_KKPP_8bins_KKPPScheme.root", "recreate");
+  TFile fOut(
+      "/home/patoc/work/DPhil1/D0_Mixing_Generator_Studies/for_jairus/"
+      "D02pipipipi_binning_scheme/ROOT_output/2M_KKPP_8bins_KKPPScheme.root",
+      "recreate");
   TTree t1("GenEvents", "Test events binned with KKPP binning");
 
   t1.Branch("BinNumber", &BinNumber, "BinNumber/I");
@@ -69,23 +75,24 @@ int main()
 
   Int_t nevents = 2000000;
 
-  for (Int_t n = 0; n < nevents; n++)
-  {
+  for (Int_t n = 0; n < nevents; n++) {
 
     Event event(generator.Generate());
-    std::vector<TLorentzVector> P = PiPiPiPiMath::ConvertEventTo4Vectors(event.GetEventVector());
+    std::vector<TLorentzVector> P =
+        PiPiPiPiMath::ConvertEventTo4Vectors(event.GetEventVector());
 
     std::array<TLorentzVector, 4> Daughters;
-    for (std::size_t i = 0; i < 4; i++)
-    {
+    for (std::size_t i = 0; i < 4; i++) {
       Daughters[i] = P[i];
     }
 
-    // TGenPhaseSpace is independent of particle ordering (Martin). Keep the ordering the same as the one for the MC to avoid confusion. 
+    // TGenPhaseSpace is independent of particle ordering (Martin). Keep the
+    // ordering the same as the one for the MC to avoid confusion.
 
     m12 = (Daughters[0] + Daughters[1]).M();
 
-    // Have to do this as the AmpGen model could not calculate the CP conjugated amplitudes. Pions ordered as + - + -
+    // Have to do this as the AmpGen model could not calculate the CP conjugated
+    // amplitudes. Pions ordered as + - + -
     std::vector<double> myEvent = event.GetEventVector();
 
     Amplitude amplitude;
@@ -98,8 +105,8 @@ int main()
     DBARamp_real = DBARamp.real();
     DBARamp_imag = DBARamp.imag();
 
-    if (TMath::IsNaN(Damp_real) || TMath::IsNaN(Damp_imag) || TMath::IsNaN(DBARamp_real) || TMath::IsNaN(DBARamp_imag))
-    {
+    if (TMath::IsNaN(Damp_real) || TMath::IsNaN(Damp_imag) ||
+        TMath::IsNaN(DBARamp_real) || TMath::IsNaN(DBARamp_imag)) {
       return 0;
     }
 
@@ -107,17 +114,19 @@ int main()
     DBARamp_Mag = sqrt(std::norm(DBARamp));
     StrongPhaseDiff = std::arg(Damp * std::conj(DBARamp));
     rD = std::norm(Damp / DBARamp);
-    
+
     int number_of_bins = 8;
 
-    BinNumber = static_cast<int>((StrongPhaseDiff + TMath::Pi()) / (2 * TMath::Pi() / number_of_bins)) + 1;
-    
+    BinNumber = static_cast<int>((StrongPhaseDiff + TMath::Pi()) /
+                                 (2 * TMath::Pi() / number_of_bins)) +
+                1;
+
     if (rD > 1) {
-        BinNumber = -(number_of_bins + 1 - BinNumber);
+      BinNumber = -(number_of_bins + 1 - BinNumber);
     }
 
-    //std::cout << "BinNumber: " << BinNumber << "\n";
-    
+    // std::cout << "BinNumber: " << BinNumber << "\n";
+
     t1.Fill();
   }
 
